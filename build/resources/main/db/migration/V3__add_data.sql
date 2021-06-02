ced2a59ee2c72836dd1fd95d1af7f1f28b06a1d8
Andrej Macko <a.macko96@gmail.com>
2021-06-02 09:56:08 +0200

    no message

---
+++ build/resources/main/db/migration/V3__add_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO TBL_FEEDBACK (email, priority, category, content, filepath,  progress, DATE) VALUES
+  ('abc@gmdsadsaail.com', 'high', 'FFG', 'dsadsadsadsadsadsadsadsadsa', 'http://localhost:9000/files/pic0.jpeg',  true, '2005-12-31 23:59:59');
